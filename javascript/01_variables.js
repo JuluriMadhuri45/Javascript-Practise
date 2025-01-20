6fe6004de5eb4ab07b9148996df953364df136a7
JuluriMadhuri45 <julurimadhuri45@gmail.com>
2025-01-20 09:49:26 +0000

    variables

---
+++ javascript/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=123456;
+let accountName="xyz";
+var accountemail="juludjdfh@gmail.com";
+const accountdate;  // const should always be initialised with some value 
+
+//changing values : 
+
+accountId-10; //const value cannot be changed.
+accountName="eyene";
+accountemail="dgjysned@gmail.com";
+
+
+console.log(accountId);
+console.log(accountName);
+console.log(accountemail);
+console.log(accountdate);
+
